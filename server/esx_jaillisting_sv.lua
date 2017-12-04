f156f83b3dd97ad699fb3e8079d807406a1dbfaf
Solaris <33255493+ShadowsX2V2@users.noreply.github.com>
2017-12-04 11:03:06 +0100

    Update esx_jaillisting_sv.lua

--- server/esx_jaillisting_sv.lua
+++ server/esx_jaillisting_sv.lua
@@ -1,13 +1,2 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
-
-
-AddEventHandler('chatMessage', function(from, name, message)
-    if message == "/news" then
-        CancelEvent()
-        TriggerClientEvent("TrafficAlert", from)
-        TriggerEvent('InteractSound_CL:PlayOnOne', 'demo', 0.1)        
-    end
-end)
